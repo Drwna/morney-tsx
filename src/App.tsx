35875ef8e71a54406ae266b92b6871041db9758f
draganet <3417237276@qq.com>
2023-03-28 18:20:09 +0800

    🦄 refactor: delete unused code
    delete unused code and convert all to TSX syntax

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { defineComponent } from 'vue'
+import { HelloWorld } from './components/helloWorld/HelloWorld'
+
+export default defineComponent({
+  setup: () => {
+    return () => (
+      <div class="page">
+        <HelloWorld />
+      </div>
+    )
+  },
+})
