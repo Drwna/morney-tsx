940738ab5fbc8a9ba8106df1b2506b7006333b82
draganet <3417237276@qq.com>
2023-04-08 23:48:39 +0800

    ✨ feat(pinia): use pinia
    Installed Pinia to enable state management in our Vue.js application.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
-import App from './App'
+import { createPinia } from 'pinia'
 import 'virtual:svg-icons-register'
+import App from './App'
 import router from '@/router/index'
 
 const app = createApp(App)
 
+const pinia = createPinia()
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
