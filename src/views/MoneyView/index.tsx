0e92c64e7ab50de28e52b853f6eeb44e9a64e249
draganet <3417237276@qq.com>
2023-05-29 17:51:25 +0800

    ✨ feat(component): FormItem component

--- src/views/MoneyView/index.tsx
+++ src/views/MoneyView/index.tsx
@@ -1,6 +1,7 @@
 import { defineComponent, ref } from 'vue'
 import { Tabs } from '@/components/Tabs'
 import { NumberPad } from '@/components/money/NumberPad'
+import { FormItem } from '@/components/money/FormItem'
 
 const recordTypeList = [
   { name: '支出', value: '-' },
@@ -11,13 +12,16 @@ const MoneyView = defineComponent({
   setup: (props, context) => {
     const num = ref<string>('0')
     const type = ref<string>('-')
+    const notes = ref<string>('')
     return () => (
-      <>
-        <div>1. 标签</div>
-        <div>2. 备注</div>
+      <div class="h-full flex flex-col">
+        <div class="flex-1 overflow-auto">
+          标签
+        </div>
+        <FormItem name='备注' v-model={notes.value} placeholder='请输入备注信息' />
         <Tabs dataSource={recordTypeList} v-model={[type.value, 'value']} />
         <NumberPad v-model={num.value} />
-      </>
+      </div>
     )
   },
 })
