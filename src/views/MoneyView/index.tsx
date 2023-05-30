5f73e628c77431e53b6e8283d08fb62df73d2cb5
draganet <3417237276@qq.com>
2023-05-30 14:28:51 +0800

    ✨ feat(money): Integrate data for Money

--- src/views/MoneyView/index.tsx
+++ src/views/MoneyView/index.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, ref } from 'vue'
+import { defineComponent, reactive } from 'vue'
 import { Tabs } from '@/components/Tabs'
 import { NumberPad } from '@/components/money/NumberPad'
 import { FormItem } from '@/components/money/FormItem'
@@ -9,20 +9,30 @@ const recordTypeList = [
   { name: '收入', value: '+' },
 ]
 
+interface ItemRecord {
+  tags: string[]
+  notes: string
+  type: string
+  amound: string
+}
+
 const MoneyView = defineComponent({
   setup: (props, context) => {
-    const num = ref<string>('0')
-    const type = ref<string>('-')
-    const notes = ref<string>('')
-    const tags = ref<string[]>(['吃饭'])
+    const tagSource = ['买衣服', '吃饭', '收租', '兼职']
+    const record = reactive<ItemRecord>({
+      tags: ['吃饭', '兼职'],
+      notes: '我是备注',
+      type: '-',
+      amound: '',
+    })
     return () => (
       <div class="h-full flex flex-col">
         <div class="flex-1 overflow-auto">
-          <Tags tagSource={['买衣服', '吃饭', '收租']} v-model:selected={tags.value} />
+          <Tags tagSource={tagSource} v-model={[record.tags, 'selected']} />
         </div>
-        <FormItem name='备注' v-model={notes.value} placeholder='请输入备注信息' />
-        <Tabs dataSource={recordTypeList} v-model={[type.value, 'value']} />
-        <NumberPad v-model={num.value} />
+        <FormItem name='备注' v-model={record.notes} placeholder='请输入备注信息' />
+        <Tabs dataSource={recordTypeList} v-model:value={record.type} />
+        <NumberPad v-model={record.amound} />
       </div>
     )
   },
