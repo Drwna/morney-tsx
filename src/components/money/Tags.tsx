c4914939ced9c06ea01aef4e65719b0c34704cc6
draganet <3417237276@qq.com>
2023-05-31 18:13:40 +0800

    🦄 refactor(Tags): Use useRecordStore
    Refactored Tags component to use the useRecordStore hook for managing tag data in the application.

--- src/components/money/Tags.tsx
+++ src/components/money/Tags.tsx
@@ -1,14 +1,15 @@
 import type { PropType } from 'vue'
 import { computed, defineComponent } from 'vue'
+import { useRecordStore } from '@/store/useRecordStore'
 
 export const Tags = defineComponent({
   props: {
     tagSource: {
-      type: Array as PropType<string[]>,
+      type: Array as PropType<Tag[]>,
       required: true,
     },
     selected: {
-      type: Array as PropType<string[]>,
+      type: Array as PropType<Tag[]>,
       default: () => [],
     },
   },
@@ -18,29 +19,41 @@ export const Tags = defineComponent({
       get() { return props.selected },
       set(value) { context.emit('update:selected', value) },
     })
-    const toggle = (tag: string) => {
-      if (selectedTags.value.includes(tag)) {
-        const index = selectedTags.value.indexOf(tag)
+    const isSelected = (tag: Tag) => {
+      for (const item of selectedTags.value) {
+        if (item.id === tag.id)
+          return true
+      }
+      return false
+    }
+    const toggle = (tag: Tag) => {
+      if (isSelected(tag)) {
+        const index = selectedTags.value.findIndex(item => item.id === tag.id)
         selectedTags.value.splice(index, 1)
       }
       else {
         selectedTags.value.push(tag)
       }
     }
+    const recordStore = useRecordStore()
+    const createTag = () => {
+      const name = window.prompt('请输入标签名')
+      recordStore.createTag(name)
+    }
     return () => (
       <div class="h-full mx-4 flex flex-col-reverse">
         <div>
-          <button class="my-2 underline text-[#999999] text-sm">新增标签</button>
+          <button onClick={createTag} class="my-2 underline text-[#999999] text-sm">新增标签</button>
         </div>
         <ul class="flex flex-1 overflow-auto gap-3 my-2 text-sm flex-wrap content-start">
           {
             props.tagSource.map((tag) => {
               return (
                 <li onClick={() => toggle(tag)} class={
-                  selectedTags.value.includes(tag)
+                  isSelected(tag)
                     ? 'h-6 leading-6 rounded-full px-5 text-[#f5f5f5] bg-[#737373]'
                     : 'h-6 leading-6 rounded-full px-5 bg-[#d9d9d9]'
-                }>{tag}</li>
+                }>{tag.name}</li>
               )
             })
           }
