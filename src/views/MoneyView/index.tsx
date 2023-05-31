aca3f3331a4de63bb542383994abe03e9e6a6173
draganet <3417237276@qq.com>
2023-05-31 18:15:21 +0800

    🦄 refactor(Money): use useRecordStore
    Refactor page to use useRecordStore hook

--- src/views/MoneyView/index.tsx
+++ src/views/MoneyView/index.tsx
@@ -1,34 +1,30 @@
 import { defineComponent, reactive } from 'vue'
+import { useRecordStore } from '@/store/useRecordStore'
 import { Tabs } from '@/components/Tabs'
 import { NumberPad } from '@/components/money/NumberPad'
 import { FormItem } from '@/components/money/FormItem'
 import { Tags } from '@/components/money/Tags'
 import RecordTypeList from '@/constants/RecordTypeList'
 
-interface ItemRecord {
-  tags: string[]
-  notes: string
-  type: string
-  amound: string
-}
-
 const MoneyView = defineComponent({
-  setup: (props, context) => {
-    const tagSource = ['买衣服', '吃饭', '收租', '兼职']
-    const record = reactive<ItemRecord>({
-      tags: ['吃饭', '兼职'],
+  setup: () => {
+    const record = reactive<RecordItem>({
+      tags: [],
       notes: '我是备注',
-      type: '-',
-      amound: '',
+      type: 'expense',
+      amount: '0',
     })
+    const saveRecord = () => {
+    }
+    const recordStore = useRecordStore()
     return () => (
       <div class="h-full flex flex-col">
         <div class="flex-1 overflow-auto">
-          <Tags tagSource={tagSource} v-model={[record.tags, 'selected']} />
+          <Tags tagSource={recordStore.tagList} v-model={[record.tags, 'selected']} />
         </div>
         <FormItem name='备注' v-model={record.notes} placeholder='请输入备注信息' />
         <Tabs dataSource={RecordTypeList} v-model:value={record.type} />
-        <NumberPad v-model={record.amound} />
+        <NumberPad v-model={record.amount} onSubmit={saveRecord} />
       </div>
     )
   },
