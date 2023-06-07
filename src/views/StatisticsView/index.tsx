fac4c8dbc087f3a39cdd151bf31a386f02e6a9e5
draganet <3417237276@qq.com>
2023-06-07 17:50:45 +0800

    ✨ feat(Statistics): Render Statistics page

--- src/views/StatisticsView/index.tsx
+++ src/views/StatisticsView/index.tsx
@@ -1,11 +1,17 @@
 import type { PropType } from 'vue'
-import { defineComponent, ref } from 'vue'
+import { computed, defineComponent, reactive, ref, watchEffect } from 'vue'
 
 import dayjs from 'dayjs'
 import { Tabs } from '@/components/Tabs'
 import RecordTypeList from '@/constants/RecordTypeList'
 import { useRecordStore } from '@/store/useRecordStore'
 import clone from '@/util/clone'
+
+type GroupeList = {
+  title: string
+  total: string
+  items: RecordItem[]
+}[]
 
 const StatisticView = defineComponent({
   props: {
@@ -16,7 +22,7 @@ const StatisticView = defineComponent({
   setup: () => {
     const recordStore = useRecordStore()
     const recordList = recordStore.recordList
-    const type = ref('income')
+    const type = ref('expense')
 
     /*
     [
@@ -36,57 +42,84 @@ const StatisticView = defineComponent({
     ]
     */
 
-    type GroupeList = {
-      title: string
-      total: string
-      items: RecordItem[]
-    }[]
+    const groupList = reactive<GroupeList>([
+      // Q: 为什么要先 push 一个空的？
+      // A: 因为下面的逻辑是拿最后一个元素来比较，如果不先 push 一个空的，那么最后一个元素就会被忽略
+      {
+        title: dayjs().format('YYYY-MM-DD'),
+        total: '0',
+        items: [],
+      },
+    ])
 
-    const newList = clone(recordList)
-      .filter(record => record.type === type.value)
-      .sort((a, b) => a.createdAt! > b.createdAt! ? -1 : 1)
+    const tagString = (tags: Tag[]) => {
+      return tags.length === 0 ? '无标签' : tags.map(t => t.name).join('，')
+    }
 
-    const groupList: GroupeList = []
+    const newList = computed(() =>
+      clone(recordList)
+        .filter(record => record.type === type.value)
+        .sort((a, b) => a.createdAt! > b.createdAt! ? -1 : 1),
+    )
 
-    groupList.push({
-      title: dayjs(newList[0]?.createdAt).format('YYYY-MM-DD'),
-      total: '0',
-      items: [],
+    const updateGroupList = () => {
+      newList.value.forEach((record) => {
+        const lastGroup = groupList[groupList.length - 1]
+        const lastGroupTitle = lastGroup.title
+        const lastGroupTotal = lastGroup.total
+        const lastGroupItems = lastGroup.items
+
+        if (dayjs(record.createdAt).format('YYYY-MM-DD') === lastGroupTitle) {
+          lastGroupItems.push(record)
+          lastGroup.total = (Number(lastGroupTotal) + Number(record.amount)).toString()
+        }
+        else {
+          groupList.push({
+            title: dayjs(record.createdAt).format('YYYY-MM-DD'),
+            total: record.amount,
+            items: [record],
+          })
+        }
+      })
+    }
+
+    watchEffect(() => {
+      Object.assign(groupList, [{
+        title: dayjs(newList.value[0]?.createdAt).format('YYYY-MM-DD'),
+        total: '0',
+        items: [],
+      }])
+      updateGroupList()
     })
-
-    newList.forEach((record) => {
-      const lastGroup = groupList[groupList.length - 1]
-      const lastGroupTitle = lastGroup.title
-      const lastGroupTotal = lastGroup.total
-      const lastGroupItems = lastGroup.items
-
-      if (dayjs(record.createdAt).format('YYYY-MM-DD') === lastGroupTitle) {
-        lastGroupItems.push(record)
-        lastGroup.total = (Number(lastGroupTotal) + Number(record.amount)).toString()
-      }
-      else {
-        groupList.push({
-          title: dayjs(record.createdAt).format('YYYY-MM-DD'),
-          total: record.amount,
-          items: [record],
-        })
-      }
-    })
-
-    console.log('newList ==>', newList)
-    console.log('groupList ==>', groupList)
 
     return () => (
       <>
         <Tabs dataSource={RecordTypeList} v-model:value={type.value} />
-        <div>
-          {/* <div> 没有记录 </div> */}
-          <div>
-            <div class="item">
-
-            </div>
-          </div>
-        </div>
+        <ol>
+          {
+            groupList.length === 0
+              ? <li class="flex justify-center items-center h-20 text-[#999]">暂无数据</li>
+              : groupList.map(group => (
+                <li>
+                  <h3 class="flex justify-between px-2 py-3 text-base">
+                    {group.title}
+                    <span>￥{group.total}</span>
+                  </h3>
+                  <ol>
+                    {
+                      group.items.map(item => (
+                        <li class="flex justify-between bg-white px-2 py-3">
+                          <span class="flex-shrink-0">{tagString(item.tags)}</span>
+                          <span class="ml-2 mr-auto pr-1 text-[#999]">{item.notes}</span>
+                          <span>￥{item.amount}</span>
+                        </li>
+                      ))
+                    }
+                  </ol>
+                </li>
+              ))
+          }
+        </ol>
       </>
     )
   },
